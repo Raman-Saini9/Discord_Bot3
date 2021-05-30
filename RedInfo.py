92bb787d5c2e338ee766ee85f9d137ec2fe345ee
Raman <68729255+Raman-Saini9@users.noreply.github.com>
2021-05-30 15:22:16 +0530

    Update RedInfo.py

--- RedInfo.py
+++ RedInfo.py
@@ -7,7 +7,7 @@ import config
 reddit = praw.Reddit( client_id= config.CLIENT_ID,
                       client_secret= config.CLIENT_SECRET ,
                       password= config.PASSWORD,
-                      user_agent= 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36',
+                      user_agent= config.AGENT,
                       username= config.USERNAME,
                       check_for_async=False)
 
